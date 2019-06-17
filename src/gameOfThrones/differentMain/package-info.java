1c4a8436bb7eb35e39a720df6dd85d1b1d56992c
jelenam86 <jelena.mat86@gmail.com>
2019-06-17 23:48:32 +0200

    Added info about main methods

---
+++ src/gameOfThrones/differentMain/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * There is more than enough methods for the tasks from the readme.md file.
+ * So this package contains classes that use those methods 
+ * and in a kind different approach resolve mentioned tasks.
+ * 
+ * Application package contains one more approach for solving task, based on GUI.
+ */
+/**
+ * @author jelena
+ *
+ */
+package gameOfThrones.differentMain;
